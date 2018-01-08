f7fa7c28b709a0530061e8bd9a766d75f397b839
Emanuel L.D. Silva <toolsjob.neodeon@gmail.com>
2018-01-08 17:14:41 -0200

    Updated File -  implemented, task end button

--- www/js/controllers/task.js
+++ www/js/controllers/task.js
@@ -1,6 +1,16 @@
 angular.module('starter').controller('ViewTask', function($scope){
-	$scope.tasks = ['Tarefa 01','Tarefa 02','Tarefa 03','Tarefa 04','Tarefa 05'];
+	$scope.tasks = [
+	{'status': 'Pendente', 'title' : 'Tarefa 01', 'nivel' : 'Importante', 'desc' : 'Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.'}
+	];
 });
 angular.module('starter').controller('SelectedTask',function($scope, $stateParams){
-	$scope.task = $stateParams.task;
+	$scope.task = angular.fromJson($stateParams.task);
+	$scope.items = [{'end' : 'Concluída'}];
+	$scope.taskEnd = function(item,end){
+		if(end.status == 'Pendente'){
+			$scope.task.status = 'Concluída';
+		}else{
+			$scope.task.status = 'Pendente';
+		}
+	}
 })
